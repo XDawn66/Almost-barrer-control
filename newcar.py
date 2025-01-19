440e6977fefacb242f644aaae8dc59d2b8b5bf31
XDawn66 <jiang1241838337@Gmail.com>
2025-01-19 06:54:37 -0800

    more tries

--- newcar.py
+++ newcar.py
@@ -194,7 +194,7 @@ class Car:
         
         reward =  distance/(CAR_SIZE_X / 2)
         if not self.is_alive():
-            reward -= 30  # Harsh penalty for crashin
+            reward -= 40  # Harsh penalty for crashin
 
         # sharp_turn_threshold = 0.3  # Example threshold for sharp curvature
         # max_speed = 30.0  # Maximum desired speed
@@ -203,9 +203,9 @@ class Car:
         # stay_center = abs(self.radars[0][1]-self.radars[1][1]) 
         # reward -= stay_center * 0.1  # Penalize deviation from center
 
-        # desired_speed = 12.0
-        # speed_penalty = 0.5 * (self.speed - desired_speed) ** 2
-        # reward -= speed_penalty
+        desired_speed = 10.0
+        speed_penalty =  (self.speed - desired_speed) ** 2
+        reward -= speed_penalty
 
         # Check if the goal is reached
         if self.check_goal_reached(self.position):
@@ -216,7 +216,7 @@ class Car:
         
         dis_from_goal = min(0,self.total_checkpoints - self.checkpoint_reached)
         reward -= dis_from_goal * 15
-        color = game_map.get_at((x, y))[:3]
+        #color = game_map.get_at((x, y))[:3]
         #if color != (0,0,0) and color !=(255, 255, 255) and color !=((28, 28, 28)):
             #print(color)
         return reward
@@ -230,96 +230,6 @@ class Car:
         rotated_image = rotated_image.subsurface(rotated_rectangle).copy()
         return rotated_image
     
-
-
-#This is the default neat ML which i didn't use
-# def run_simulation(genomes, config):
-    
-#     # Empty Collections For Nets and Cars
-#     nets = []
-#     cars = []
-
-#     # Initialize PyGame And The Display
-#     pygame.init()
-#     screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
-
-#     # For All Genomes Passed Create A New Neural Network
-#     for i, g in genomes:
-#         # net = neat.nn.FeedForwardNetwork.create(g, config)
-#         # nets.append(net)
-#         g.fitness = 0
-
-#         cars.append(Car())
-
-#     # Clock Settings
-#     # Font Settings & Loading Map
-#     clock = pygame.time.Clock()
-#     generation_font = pygame.font.SysFont("Arial", 30)
-#     alive_font = pygame.font.SysFont("Arial", 20)
-#     game_map = pygame.image.load('map2.png').convert() # Convert Speeds Up A Lot
-
-#     global current_generation
-#     current_generation += 1
-
-#     # Simple Counter To Roughly Limit Time (Not Good Practice)
-#     counter = 0
-
-#     while True:
-#         # Exit On Quit Event
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 sys.exit(0)
-
-#         # For Each Car Get The Acton It Takes
-#         for i, car in enumerate(cars):
-#             output = nets[i].activate(car.get_data())
-#             choice = output.index(max(output))
-#             if choice == 0:
-#                 car.angle += 10 # Left
-#             elif choice == 1:
-#                 car.angle -= 10 # Right
-#             elif choice == 2:
-#                 if(car.speed - 2 >= 12):
-#                     car.speed -= 2 # Slow Down
-#             else:
-#                 car.speed += 2 # Speed Up
-        
-#         # Check If Car Is Still Alive
-#         # Increase Fitness If Yes And Break Loop If Not
-#         still_alive = 0
-#         for i, car in enumerate(cars):
-#             if car.is_alive():
-#                 still_alive += 1
-#                 car.update(game_map)
-#                 genomes[i][1].fitness += car.get_reward()
-
-#         if still_alive == 0:
-#             break
-
-#         counter += 1
-#         if counter == 30 * 40: # Stop After About 20 Seconds
-#             break
-
-#         # Draw Map And All Cars That Are Alive
-#         screen.blit(game_map, (0, 0))
-#         for car in cars:
-#             if car.is_alive():
-#                 car.draw(screen)
-        
-#         # Display Info
-#         text = generation_font.render("Generation: " + str(current_generation), True, (0,0,0))
-#         text_rect = text.get_rect()
-#         text_rect.center = (900, 450)
-#         screen.blit(text, text_rect)
-
-#         text = alive_font.render("Still Alive: " + str(still_alive), True, (0, 0, 0))
-#         text_rect = text.get_rect()
-#         text_rect.center = (900, 490)
-#         screen.blit(text, text_rect)
-
-#         pygame.display.flip()
-#         clock.tick(60) # 60 FPS
-
     def runsimulation(self):
         #a timer to force stop
         total_steps = 50000
@@ -338,16 +248,16 @@ class Car:
         #seting up the SAC
         env = Env.CarEnv(game_map,screen)
         #loaded the model I trained so far
-        #model = SAC.load("MlpPolicy_try_7",env, tensorboard_log="./sac_car_env/")
+        model = SAC.load("MlpPolicy_try_6",env, tensorboard_log="./sac_car_env/")
         #pygame.init()  # Initialize Pygame
         #this is for creating a new model
         #model = SAC("MlpPolicy", env,verbose=1, tensorboard_log="./sac_car_env/")
         #model.load("MlpPolicy3")
 
         # Train the agent for a set number of timesteps
-        model.learn(total_timesteps=total_steps, tb_log_name="SAC_run")
+        #model.learn(total_timesteps=total_steps, tb_log_name="SAC_run")
         # Save the trained model 
-        model.save("MlpPolicy_try_8")
+        #model.save("MlpPolicy_try_11")
 
         # Initialize previous position
         self.previous_position = self.position
